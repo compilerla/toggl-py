10e5dd1ddde913035c40db1c78f5ff36c1ef2c2d
Kegan Maher <kegan@compiler.la>
2024-04-29 16:09:50 +0000

    feat(codegen): stub pytests for api and models
    tests need implementation

--- codegen/TogglPythonClientCodegen/src/main/java/la/compiler/codegen/TogglPythonClientCodegen.java
+++ codegen/TogglPythonClientCodegen/src/main/java/la/compiler/codegen/TogglPythonClientCodegen.java
@@ -6,11 +6,14 @@ import io.swagger.codegen.*;
 import io.swagger.codegen.languages.PythonClientCodegen;
 
 public class TogglPythonClientCodegen extends PythonClientCodegen {
+    private String testFolder;
+
     public TogglPythonClientCodegen() {
         super();
 
         modelPackage = "models";
         apiPackage = "api";
+        testFolder = "tests";
 
         setLibrary("urllib3");
     }
@@ -39,6 +42,13 @@ public class TogglPythonClientCodegen extends PythonClientCodegen {
                 "__init__.py"));
         supportingFiles.add(new SupportingFile("rest.mustache", packageFolder, "rest.py"));
 
+        supportingFiles.add(new SupportingFile("__init__test.mustache", testFolder, "__init__.py"));
+        supportingFiles.add(new SupportingFile("conftest.mustache", testFolder, "conftest.py"));
+        supportingFiles.add(new SupportingFile("__init__test.mustache", testFolder + File.separatorChar + apiPackage,
+                "__init__.py"));
+        supportingFiles.add(new SupportingFile("__init__test.mustache", testFolder + File.separatorChar + modelPackage,
+                "__init__.py"));
+
         modelPackage = packageName + "." + modelPackage;
         apiPackage = packageName + "." + apiPackage;
     }
@@ -46,5 +56,15 @@ public class TogglPythonClientCodegen extends PythonClientCodegen {
     @Override
     public String getName() {
         return "TogglPythonClientCodegen";
+    }
+
+    @Override
+    public String apiTestFileFolder() {
+        return outputFolder + File.separatorChar + testFolder + File.separatorChar + "api";
+    }
+
+    @Override
+    public String modelTestFileFolder() {
+        return outputFolder + File.separatorChar + testFolder + File.separatorChar + "models";
     }
 }
